c9b609734db332ba3544f4e262f36fc5a277bbd2
Arne Neumann <nlpbox.programming@arne.cl>
2021-01-16 22:01:37 +0100

    nginx-certbot: only use one domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(arne.cl rst-workbench.arne.cl)
+domains=(rst-workbench.arne.cl)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="rst-workbench.nginx@arne.cl" # Adding a valid address is strongly recommended
